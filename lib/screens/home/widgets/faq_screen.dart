2982b8c701faefd3721f161085730361db37e872
rudolph <anakmanusia245@gmail.com>
2026-04-15 14:48:38 +0700

    disable import go_router

--- lib/screens/home/widgets/faq_screen.dart
+++ lib/screens/home/widgets/faq_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+// import 'package:go_router/go_router.dart';
 import '../../../core/app_colors.dart';
 import '../../shared/custom_navbar.dart';
 
